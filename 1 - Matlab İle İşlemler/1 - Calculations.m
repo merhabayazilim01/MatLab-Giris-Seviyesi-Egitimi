ba25f7dab7739f5709aaec2eb82d11a3f2f71d34
Barış Ertemli <ertemlibaris@gmail.com>
2023-12-27 13:56:29 +0100

    MatlabCalculations Video İçerikleri

---
+++ 1 - Matlab İle İşlemler/1 - Calculations.m
@@ -0,0 +1,34 @@
+% Matlab'i hesap makinesi olarak kullanmayı öğrendiğimiz dosya!
+
+%% Matematiksel İşlemler
+5+3 % Toplama
+5-3 % Çıkarma
+5/2 % Bölme
+5*2 % Çarpma
+5^2 % Üssel işlem
+%% Örnek 1
+(5 + 2)*2^2 + 3
+
+%% Örnek 2
+((4 + 2*2)/2)^2
+
+%% Örnek 3
+6*(10/13) + 18/(5*7) + (9^2)*5
+
+%% Örnek 4
+3*(35^(1/4)) + 14^(0.35)
+
+%% Kullanışlı Matematiksel Fonksiyonlar
+sqrt(4) % Karekök, square root'un kısaltması.
+exp(2) % Üssel fonksiyon, logaritma fonksiyonunun tersi.
+
+% Trigonometrik Fonksiyonlar
+cos(1) % Değer olarak RADYAN alır.
+cosd(90) % Değer olarak DERECE alır.
+sin(1) % Değer olarak RADYAN alır.
+sind(180) % Değer olarak DERECE alır.
+
+%% Karmaşık Sayılar
+real(1+2i) % Karmaşık sayının gerçek kısmını bulur.
+imag(1+2i) % Karmaşık sayının karmaşık kısmını bulur.
+sqrt(-1) % i'yi böyle de bulabiliyoruz.
